e637306363aaac6013fd64808577addc4e9b8943
landrea-velez <landrea.velez@gmail.com>
2025-06-20 10:57:30 -0500

    creando el router

--- src/main/java/com/landreavelez/springboot/webflux/client/app/RouterConfig.java
+++ src/main/java/com/landreavelez/springboot/webflux/client/app/RouterConfig.java
@@ -1,5 +1,21 @@
 package com.landreavelez.springboot.webflux.client.app;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import com.landreavelez.springboot.webflux.client.app.handler.ProductoHandler;
+
+@Configuration
 public class RouterConfig {
+	
+	@Bean
+	public RouterFunction<ServerResponse> rutas(ProductoHandler handler){
+		return route(GET("/api/client"), handler::listar)
+				.andRoute(GET("/api/client/{id}"), handler::ver);
+	}
 
 }
